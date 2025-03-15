cbe17d7717cbcd608efcb193546dfa6edb5a87d3
Hidetake Iwata <int128@gmail.com>
2025-03-15 15:38:58 +0900

    Initial import

--- src/run.ts
+++ src/run.ts
@@ -1,10 +1,44 @@
+import assert from 'assert'
 import * as core from '@actions/core'
+import { Octokit } from '@octokit/rest'
+import { getOctokit } from './github.js'
 
 type Inputs = {
-  name: string
+  appId: string
+  appPrivateKey: string
+  dryRun: boolean
 }
 
-// eslint-disable-next-line @typescript-eslint/require-await
 export const run = async (inputs: Inputs): Promise<void> => {
-  core.info(`my name is ${inputs.name}`)
+  const octokit = getOctokit({
+    type: 'app',
+    appId: inputs.appId,
+    privateKey: inputs.appPrivateKey,
+  })
+  const { data: authenticated } = await octokit.rest.apps.getAuthenticated()
+  assert(authenticated)
+  core.info(`Authenticated as ${authenticated.name}`)
+  core.summary.addHeading('actions-tanpopo-bot summary', 2)
+  await processInstallations(inputs, octokit)
+}
+
+const processInstallations = async (inputs: Inputs, octokit: Octokit) => {
+  const installations = await octokit.paginate(octokit.apps.listInstallations, { per_page: 100 })
+  for (const installation of installations) {
+    core.info(`Processing the installation ${installation.id}`)
+    await processInstallation(inputs, installation.id)
+  }
+}
+
+const processInstallation = async (inputs: Inputs, installationId: number) => {
+  const octokit = getOctokit({
+    type: 'installation',
+    appId: inputs.appId,
+    privateKey: inputs.appPrivateKey,
+    installationId,
+  })
+  const repositories = await octokit.paginate(octokit.rest.apps.listReposAccessibleToInstallation, { per_page: 100 })
+  for (const repository of repositories) {
+    core.info(`Processing the repository ${repository.owner.login}`)
+  }
 }
