b0f277f2f57c05171d73715895d893823434baae
Hidetake Iwata <int128@gmail.com>
2025-03-16 22:04:51 +0900

    Fix

--- tests/run.test.ts
+++ tests/run.test.ts
@@ -1,15 +1,7 @@
 import { describe, expect, it } from 'vitest'
-import { findCheckedRepositories } from '../src/run.js'
 
-describe('findCheckedRepositories', () => {
-  it('returns checked repositories', () => {
-    const comment = `<!-- int128/actions-tanpopo-bot -->
-## :robot: actions-tanpopo-bot
-- [x] int128/foo
-- [ ] int128/bar
-- [x] int128/baz
-`
-    const checked = findCheckedRepositories(comment)
-    expect(checked).toEqual(['int128/foo', 'int128/baz'])
+describe('TODO', () => {
+  it('TODO', () => {
+    expect(true).toBe(true)
   })
 })
