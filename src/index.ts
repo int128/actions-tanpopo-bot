972e0670ba690703548d9b9642e8b8c1ce1903f0
Hidetake Iwata <int128@gmail.com>
2025-05-03 14:43:13 +0900

    Refactor context payload

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import * as core from '@actions/core'
 import { run } from './run.js'
+import { getContext, getOctokit } from './github.js'
 
 try {
-  await run()
+  const octokit = getOctokit()
+  const context = await getContext()
+  await run(octokit, context)
 } catch (e) {
   core.setFailed(e instanceof Error ? e : String(e))
   console.error(e)
